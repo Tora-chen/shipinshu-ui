6a701e8c7cd9709abd48775db55428630ca31e3c
陈志虎 <Tora_Chen@outlook.com>
2025-01-28 09:40:03 +0800

    添加包和api

--- src/config/api.ts
+++ src/config/api.ts
@@ -5,4 +5,6 @@ export const API_URLS = {
     getUserCollection: `${API_BASE_URL}/user-collection/my`,
     getRecommendation: `${API_BASE_URL}/lectures/recommendedLecture`,
     getMyLectures: `${API_BASE_URL}/lectures/my`,
+    getLectureVideos: (lectureId: number) => `${API_BASE_URL}/lectures/${lectureId}/videos`,
+    getVideoStream: (videoId: number) => `${API_BASE_URL}/videos/${videoId}`,
 };
